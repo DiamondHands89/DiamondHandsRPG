a6cbe7d4074222cdd4608ae9030b7900474af27e
DiamondHands89 <kwamemullins@outlook.com>
2024-12-02 12:37:19 -0500

    Update EquipmentSystem.mjs

---
+++ EquipmentSystem.mjs
@@ -0,0 +1,29 @@
+import { character } from '.UserCharacter.mjs';
+
+// Handles Weapons
+
+    // Function to add weapon to character's equipment
+    
+
+
+// Handles Tools
+
+    // Function to add tool to character's equipment
+
+
+// Handles Head Equipment
+
+    // Function to add head equip to character's equipment
+
+
+// Handles Body Equipment
+
+
+// Handles Hands Equipment
+
+
+// Handles Feet Equiment
+
+
+
+
