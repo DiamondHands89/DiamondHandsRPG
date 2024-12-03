a22975d6c9961ce0ac2f1b1ebfbfe55bd6041a45
DiamondHands89 <kwamemullins@outlook.com>
2024-12-02 23:11:04 -0500

    Update EquipmentSystem.mjs
    Weapons slot

--- EquipmentSystem.mjs
+++ EquipmentSystem.mjs
@@ -1,7 +1,15 @@
 import { character } from '.UserCharacter.mjs';
 
 // Handles Weapons
-
+class Weapon {
+    constructor(name, level, damage, description, associatedSkills) {
+        this.name = name;
+        this.level = level || 0;
+        this.damage = damage || 0;
+        this.description = description || "";
+        this.associatedSkills = [];
+    }
+}
     // Function to add weapon to character's equipment
     
 
